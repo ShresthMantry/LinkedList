7d5a06f054dca5bd8f8289dd9873c5d92bb5ccde
Shresth Mantry <shresthmantry72003@gmail.com>
2023-05-07 20:06:29 +0530

    modified rotate

--- practice_doublell.c
+++ practice_doublell.c
@@ -1,18 +1,17 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-
-typedef struct Node_t {
-    int data;
-    struct Node_t* next;
-    struct Node_t* prev;
-} Node_t,* Node;
-
-
-Node newNode(int data,Node next,Node prev)
+typedef struct Node_t
 {
-    Node node = (Node)calloc(1,sizeof(Node_t));
+    int data;
+    struct Node_t *next;
+    struct Node_t *prev;
+} Node_t, *Node;
+
+Node newNode(int data, Node next, Node prev)
+{
+    Node node = (Node)calloc(1, sizeof(Node_t));
     node->data = data;
     node->next = next;
     node->prev = prev;
@@ -22,43 +21,43 @@ Node newNode(int data,Node next,Node prev)
 
 void printList(Node node)
 {
-    while(node)
+    while (node)
     {
         // printf("%d <- |%d| -> %d ",node->prev!=NULL?node->prev->data:-1,node->data,node->next!=NULL?node->next->data:-1);
-        printf("%d ",node->data);
+        printf("%d ", node->data);
         node = node->next;
     }
     printf("NULL\n");
 }
 
-void addFirst(Node *pnode,int data)
+void addFirst(Node *pnode, int data)
 {
-    Node temp = newNode(data,*pnode,NULL);
-    if(*pnode==NULL)
+    Node temp = newNode(data, *pnode, NULL);
+    if (*pnode == NULL)
     {
         *pnode = temp;
-        return ;
+        return;
     }
     (*pnode)->prev = temp;
     *pnode = temp;
 }
 
-void addlast(Node *pnode,int data)
+void addlast(Node *pnode, int data)
 {
-    if(*pnode==NULL)
+    if (*pnode == NULL)
     {
-        addFirst(pnode,data);
+        addFirst(pnode, data);
         return;
     }
-    
+
     Node node = *pnode;
-    while(node->next)
+    while (node->next)
     {
         node = node->next;
     }
 
-    node->next=newNode(data,NULL,node);;
-
+    node->next = newNode(data, NULL, node);
+    ;
 }
 
 Node reverse(Node head)
@@ -66,44 +65,42 @@ Node reverse(Node head)
     Node node = head;
     Node next = NULL;
     Node prev = node->prev;
-    while(node)
+    while (node)
     {
         next = node->next;
-        node->next=node->prev;
-        node->prev=next;
+        node->next = node->prev;
+        node->prev = next;
         prev = node;
         node = next;
-
     }
 
     return prev;
 }
 
-Node reverseK(Node head,int k)
+Node reverseK(Node head, int k)
 {
 
-    if(head == NULL)
-    return NULL;
+    if (head == NULL)
+        return NULL;
 
     Node curr = head;
     Node next = NULL;
     Node previous = NULL;
     int count = 1;
-    while(curr && count<=k)
+    while (curr && count <= k)
     {
         next = curr->next;
-        curr->next =  previous;
+        curr->next = previous;
         curr->prev = next;
         previous = curr;
         curr = next;
         count++;
-
     }
 
     previous->prev = NULL;
-    head->next = reverseK(curr,k);
-    if(head->next)
-    head->next->prev = head;
+    head->next = reverseK(curr, k);
+    if (head->next)
+        head->next->prev = head;
 
     return previous;
 }
@@ -111,59 +108,57 @@ Node reverseK(Node head,int k)
 void sort(Node head)
 {
     Node curr = head;
-    while(curr)
+    while (curr)
     {
         Node node = curr->next;
-        while(node)
+        while (node)
         {
-            if(curr->data>node->data)
+            if (curr->data > node->data)
             {
                 int temp = curr->data;
-                curr->data=node->data;
-                node->data=temp;
+                curr->data = node->data;
+                node->data = temp;
             }
-            node=node->next;
+            node = node->next;
         }
-        curr=curr->next;
+        curr = curr->next;
     }
 }
 
-void sortK(Node head,int size)
+void sortK(Node head, int size)
 {
 
-    if(head==NULL)
-    return ;
+    if (head == NULL)
+        return;
 
     Node curr = head;
     int k = 1;
-    while(curr && k<=size)
+    while (curr && k <= size)
     {
         Node node = curr->next;
-        int count = size-k;
-        while(node && count>0)
+        int count = size - k;
+        while (node && count > 0)
         {
-            if(curr->data>node->data)
+            if (curr->data > node->data)
             {
                 int temp = curr->data;
-                curr->data=node->data;
-                node->data=temp;
+                curr->data = node->data;
+                node->data = temp;
             }
-            node=node->next;
+            node = node->next;
         }
-        curr=curr->next;
+        curr = curr->next;
         k++;
-    } 
+    }
 
-    sortK(curr,size)  ;
-
+    sortK(curr, size);
 }
-
 
 int getLen(Node head)
 {
     Node node = head;
     int count = 0;
-    while(node)
+    while (node)
     {
         count++;
         node = node->next;
@@ -171,46 +166,46 @@ int getLen(Node head)
     return count;
 }
 
-Node rotate(Node head,int k)
+Node rotate(Node head, int k)
 {
     Node node = head;
-    int count = getLen(head)-k;
+    k = k % (getLen(head));
+    if(k==0)
+    return head;
+    int count = getLen(head) - k;
     int x = 0;
     Node temp = NULL;
-    while(node->next)
+    while (node->next)
     {
         x++;
-        if(x==count)
+        if (x == count)
         {
-            temp=node;
+            temp = node;
         }
         node = node->next;
     }
 
     node->next = head;
-    head->prev=node;
+    head->prev = node;
 
-    Node newHead= temp->next;
-    temp->next=NULL;
-    newHead->prev=NULL;
+    Node newHead = temp->next;
+    temp->next = NULL;
+    newHead->prev = NULL;
     return newHead;
 }
-
 
 int main()
 {
     Node head = NULL;
-    addlast(&head,60);
-    addlast(&head,10);
-    addlast(&head,30);
-    addlast(&head,30);
-    addlast(&head,90);
-    addlast(&head,70);
+    addlast(&head, 60);
+    addlast(&head, 10);
+    addlast(&head, 30);
+    addlast(&head, 30);
+    addlast(&head, 90);
+    addlast(&head, 70);
 
-    sort(head);
     printList(head);
-    printList(rotate(head,3));
-
+    printList(rotate(head,7));
 }
 
-//done assignment4
+// done assignment4
